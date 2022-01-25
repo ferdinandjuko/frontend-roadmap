7d06022226cbef5d62a0548ca2a6a35cce040620
raja <raja.rakoto7@gmail.com>
2022-01-25 17:28:09 +0300

    [tips]: add switch

---
+++ javascript/tips/switch.js
@@ -0,0 +1,13 @@
+switch (key) {
+	case value:
+		//code
+		break;
+	case value:
+		//code
+		break;
+	case value:
+		//code
+		break;
+	default:
+		null; //TIPS: retourner null si les autres cas ne sont pas verifiEs
+}
