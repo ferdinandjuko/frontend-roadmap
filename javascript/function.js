46a1a06d216c4be1dd6cc4b46de910882a2bed14
raja <raja.rakoto7@gmail.com>
2022-03-07 09:44:46 +0300

    update nameFormat()

--- javascript/function.js
+++ javascript/function.js
@@ -1,40 +1,14 @@
-/*
-BANK TRANSACTION
-*/
+//Exemple de fonction qui transforme le le nom et le premier lettre du prenom en Majuscule
 
-class Account {
-	constructor(owner, accountType, connexion) {
-		this.owner = owner;
-		this.accountType = accountType;
-		this.connexion = connexion;
-	}
-
-	//function|methode (+notation)
-	sendWelcomeMessage = user => {
-		if (this.connexion === true && this.owner === user) {
-			console.log('Bienvenue ' + user + ' !');
-		} else {
-			console.log('Erreur de connexion !');
-		}
-	};
-}
-
-let my_account = new Account('Rakoto', 'Normal', true);
-my_account.sendWelcomeMessage('Rakoto');
-
-// ================================================================
-
-/*
-NAME FORMAT
-*/
-
-let firstName = 'rakotonirina';
-let lastName = 'raja';
+let firstName = 'RajA';
+let lastName = 'RakOTOnirinA';
 
 autoFormatName = (firstName, lastName) => {
-	firstName = firstName.toUpperCase();
-	lastName = lastName[0].toUpperCase() + lastName.substr(1);
-	return firstName + ' ' + lastName;
+	firstName =
+		firstName[0].toUpperCase() +
+		firstName.substr(1, firstName.length).toLowerCase();
+	lastName = lastName.toUpperCase();
+	return lastName + ' ' + firstName;
 };
 
-console.log(autoFormatName(firstName, lastName));
+console.log(autoFormatName(firstName, lastName)); //RAKOTONIRINA Raja
