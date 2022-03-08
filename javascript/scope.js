7f4bed9bcebc458118f7b7aa05fac388587ba62e
raja <raja.rakoto7@gmail.com>
2022-03-08 16:52:05 +0300

    scope

---
+++ javascript/scope.js
@@ -0,0 +1,26 @@
+{
+	//---PARENT1
+	var global = 'everywhere !'; //cette variable est accessible partout (global)
+
+	{
+		//--ENFANT1
+		let local = 'alone !';
+		console.log(global); //success
+		console.log(local); //success
+	}
+
+	console.log(global); //success
+	console.log(local); //erreur 
+}
+
+{
+	//---PARENT2
+	let local = 'alone !'; //ceci est accessible dans enfant2 puisqu'elle est dans parent2
+	console.log(global); //sucess
+  
+	{
+		//--ENFANT2
+		console.log(global); //sucess
+		console.log(local); //sucess
+	}
+}
